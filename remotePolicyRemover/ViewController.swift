d01bc21c828f00d8a0049a7dfaa031b35d6824d9
Leslie Helou <leslie@ccs-indy.com>
2018-11-30 21:59:24 -0500

    Initial Commit

---
+++ remotePolicyRemover/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  remotePolicyRemover
+//
+//  Created by Leslie Helou on 11/30/18.
+//  Copyright © 2018 jamf. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
