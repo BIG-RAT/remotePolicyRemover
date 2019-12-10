c7188ebbd6fc0b41e6d131c059289a67754a8960
leslie <leslie@ccs-indy.com>
2019-12-10 15:00:46 -0500

    changed concurrent operations on remove queue to 3

--- remotePolicyRemover/ViewController.swift
+++ remotePolicyRemover/ViewController.swift
@@ -8,8 +8,91 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
+class ViewController: NSViewController, URLSessionDelegate {
 
+    @IBOutlet weak var jamfServer_TextField: NSTextField!
+    @IBOutlet weak var username_TextField: NSTextField!
+    @IBOutlet weak var password_TextFiled: NSSecureTextField!
+    
+    @IBOutlet weak var remotePolicyCount_TextField: NSTextField!
+    @IBOutlet weak var oldestPolicy_TextField: NSTextField!
+    @IBOutlet weak var cutoffDate_TextField: NSTextField!
+    
+    @IBOutlet weak var spinner: NSProgressIndicator!
+    
+    let theOpQ = OperationQueue() // create operation queue for API calls
+    
+    var serverURL         = ""
+    var username          = ""
+    var password          = ""
+    var userCreds         = ""
+    var userBase64Creds   = ""
+    var policyIdNamdDict:[Int:Double] = [:]   // something like policyId, epoch of policy
+    var oldestRemotePolicyEpoch  = NSDate().timeIntervalSince1970
+    var oldestRemotePolicyString = ""
+    var policyCutOffDate         = ""
+    var policyCutOffEpoch:Double = 0.0
+    var currentPolicyCount       = 0
+    
+    var monthString = ""
+    var dayString   = ""
+    
+    var remotePolicyList  = [String]()
+    var remotePolicyCount:Int16 = 0
+    var httpStatusCode: Int = 0
+    
+    @IBAction func fetch_Button(_ sender: Any) {
+        remotePolicyCount = 0
+        policyIdNamdDict.removeAll()
+        remotePolicyCount_TextField.stringValue = ""
+        oldestPolicy_TextField.stringValue = ""
+        
+        username = self.username_TextField.stringValue
+        password = self.password_TextFiled.stringValue
+        
+        userCreds = "\(username):\(password)"
+        userBase64Creds = userCreds.data(using: .utf8)?.base64EncodedString() ?? ""
+        
+        spinner.startAnimation(self)
+        
+        getEndpoints(endpoint: "policies") {
+            (result: String) in
+            print("done")
+        }
+
+    }
+    @IBAction func clear_Button(_ sender: Any) {
+        
+        policyCutOffDate = cutoffDate_TextField.stringValue
+        if policyCutOffDate != "" {
+            policyCutOffEpoch = toEpochTime(dateString: policyCutOffDate)
+            print("policyCutOffEpoch: \(policyCutOffEpoch)")
+            if policyCutOffEpoch < 0.0 {
+                alert_dialog(header: "Alert:", message: "Invalid date.  Use YYYY-MM-DD")
+                return
+            }
+        } else {
+            policyCutOffEpoch = 0.0
+        }
+
+        spinner.startAnimation(self)
+        
+        for (id, epoch) in policyIdNamdDict {
+            currentPolicyCount += 1
+//            print("checking policy id: \(id) \t epoch: \(epoch)")
+            if (epoch < policyCutOffEpoch) || (policyCutOffEpoch == 0.0) {
+//                print("\tremoving policy id: \(id) \t epoch: \(epoch)")
+                RemoveRemotePolicies(endpointType: "policy", policyId: id, policyName: "", currentPolicy: currentPolicyCount, policyCount: policyIdNamdDict.count)
+            } else if currentPolicyCount == policyIdNamdDict.count {
+                RemoveRemotePolicies(endpointType: "policy", policyId: 0, policyName: "", currentPolicy: currentPolicyCount, policyCount: policyIdNamdDict.count)
+            }
+        }   // for (id, epoch) - end
+    }
+    @IBAction func quit_Button(_ sender: Any) {
+        NSApplication.shared.terminate(self)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,7 +104,263 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
+    
+//    let jamfServer = "https://m.hickoryhillseast.net"
+    
+    func getEndpoints(endpoint: String, completion: @escaping (_ result: String) -> Void) {
+        print("Getting \(endpoint)")
+        theOpQ.maxConcurrentOperationCount = 1
+        let semaphore = DispatchSemaphore(value: 0)
+        
+        self.serverURL = "\(self.jamfServer_TextField.stringValue)/JSSResource/\(endpoint)"
+//        print("initial URL: \(self.serverURL)\n")
+        self.serverURL = self.serverURL.replacingOccurrences(of: "//JSSResource", with: "/JSSResource")
+//        print("URL: \(self.serverURL)\n")
+        
+        theOpQ.addOperation {
+            let encodedURL = NSURL(string: self.serverURL)
+            let request = NSMutableURLRequest(url: encodedURL! as URL)
+            request.httpMethod = "GET"
+            let configuration = URLSessionConfiguration.default
+            configuration.httpAdditionalHeaders = ["Authorization" : "Basic \(self.userBase64Creds)", "Content-Type" : "application/json", "Accept" : "application/json"]
+            let session = Foundation.URLSession(configuration: configuration, delegate: self, delegateQueue: OperationQueue.main)
+            let task = session.dataTask(with: request as URLRequest, completionHandler: {
+                (data, response, error) -> Void in
+                if let httpResponse = response as? HTTPURLResponse {
+//                    print("httpResponse: \(String(describing: response))")
 
+                    let json = try? JSONSerialization.jsonObject(with: data!, options: .allowFragments)
+                        if let endpointJSON = json as? [String: Any] {
+                            print("endpointJSON: \(endpointJSON))")
+                            print("\n-------- Getting all \(endpoint) --------")
 
+                                print("processing policies")
+                                if let endpointInfo = endpointJSON[endpoint] as? [Any] {
+                                    let policyCount: Int = endpointInfo.count
+                                    print("\(endpoint) found: \(policyCount)")
+                                    
+                                    if policyCount > 0 {
+                                        for i in (0..<policyCount) {
+                                            let record = endpointInfo[i] as! [String : AnyObject]
+                                            let xmlID: Int = (record["id"] as! Int)
+                                            let xmlName: String = (record["name"] as! String)
+                                            //print("\(xmlName)")
+                                            self.remotePolicyList.append(xmlName)
+//                                            let regexSearch = try! NSRegularExpression(pattern: "[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9] at", options:.caseInsensitive)
+                                            
+                                            if let range = xmlName.range(of:"[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9] at", options: .regularExpression) {
+                                                let result = xmlName.substring(with:range)
+//                                                print("\tresult: \(xmlName)")
+                                                self.remotePolicyCount += 1
+                                                DispatchQueue.main.async {
+                                                    self.remotePolicyCount_TextField.stringValue = "\(self.remotePolicyCount)"
+                                                }
+                                                let currentPolicyDate = self.toEpochTime(dateString: xmlName)
+                                                self.policyIdNamdDict[xmlID] = currentPolicyDate
+//                                                usleep(300000)
+                                                
+                                            }
+                                        }
+                                    }   // end if let buildings, departments...
+                                    
+//                                    print("\(self.policyIdNamdDict)")
+                                    self.oldestPolicy_TextField.stringValue = "\(String(describing: self.oldestRemotePolicyString))"
+                                    self.spinner.stopAnimation(self)
+                                    
+                                }   //if let endpointInfo = endpointJSON - end
+  
+                        }   // if let endpointJSON - end
+                    
+                    
+                    if httpResponse.statusCode >= 199 && httpResponse.statusCode <= 299 {
+                        //print(httpResponse.statusCode)
+                    } else {
+                        // something went wrong
+                        //self.writeToHistory(stringOfText: "**** \(self.getName(endpoint: endpointType, objectXML: endPointXML)) - Failed\n")
+                        print("\n\n---------- status code ----------")
+                        print(httpResponse.statusCode)
+                        self.httpStatusCode = httpResponse.statusCode
+                        print("---------- status code ----------")
+                        print("\n\n---------- response ----------")
+                        print(httpResponse)
+                        print("---------- response ----------\n\n")
+                        switch self.httpStatusCode {
+                        case 401:
+                            print("401")
+                        //self.alert_dialog(header: "Authentication Failure", message: "Please verify username and password for the source server.")
+                        default:
+                            print("default")
+                            //self.alert_dialog(header: "Error", message: "An unknown error occured trying to query the source server.")
+                        }
+                        
+                        //                        401 - wrong username and/or password
+                        //                        409 - unable to create object; already exists or data missing or xml error
+                        //                        self.go_button.isEnabled = true
+                        self.spinner.stopAnimation(self)
+                        return
+                        
+                    }   // if httpResponse/else - end
+                }   // if let httpResponse - end
+                semaphore.signal()
+                if error != nil {
+                }
+            })  // let task = session - end
+            //print("GET")
+            task.resume()
+            semaphore.wait()
+        }   // theOpQ - end
+        
+        completion("Got endpoint - \(endpoint)")
+    }
+    
+    func RemoveRemotePolicies(endpointType: String, policyId: Int, policyName: String, currentPolicy: Int, policyCount: Int) {
+        // this is where we delete the endpoint
+        var removeDestUrl = ""
+        theOpQ.maxConcurrentOperationCount = 3
+        let semaphore = DispatchSemaphore(value: 0)
+        
+        if policyName != "All Managed Clients" && policyName != "All Managed Servers" && policyName != "All Managed iPads" && policyName != "All Managed iPhones" && policyName != "All Managed iPod touches" {
+            
+            removeDestUrl = "\(self.jamfServer_TextField.stringValue)/JSSResource/policies/id/\(policyId)"
+            removeDestUrl = removeDestUrl.replacingOccurrences(of: "//JSSResource", with: "/JSSResource")
+            
+            theOpQ.addOperation {
+                
+//                print("[RemoveEndpoints] removing \(endpointType) with ID \(policyId)  -  Object \(currentPolicy) of \(policyCount)\n")
+//                print("\n[RemoveEndpoints] removal URL: \(removeDestUrl)\n")
+                
+                let encodedURL = NSURL(string: removeDestUrl)
+                let request = NSMutableURLRequest(url: encodedURL! as URL)
+                request.httpMethod = "DELETE"
+                let configuration = URLSessionConfiguration.default
+                configuration.httpAdditionalHeaders = ["Authorization" : "Basic \(self.userBase64Creds)", "Content-Type" : "text/xml", "Accept" : "text/xml"]
+                //request.httpBody = encodedXML!
+                let session = Foundation.URLSession(configuration: configuration, delegate: self, delegateQueue: OperationQueue.main)
+                let task = session.dataTask(with: request as URLRequest, completionHandler: {
+                    (data, response, error) -> Void in
+                    if let httpResponse = response as? HTTPURLResponse {
+                        //print(httpResponse.statusCode)
+                        //print(httpResponse)
+                        DispatchQueue.main.async {
+//                            self.migrationStatus(endpoint: endpointType, count: policyCount)
+//                            self.objects_completed_field.stringValue = "\(currentPolicy)"
+//                            print("[RemoveEndpoints] Removing \(endpointType)\n")
+                        }
+                        if httpResponse.statusCode >= 199 && httpResponse.statusCode <= 299 {
+//                            print("\t[RemoveEndpoints] \(policyName)\n")
+                            self.remotePolicyCount -= 1
+                            self.policyIdNamdDict.removeValue(forKey: policyId)
+                            DispatchQueue.main.async {
+                                self.remotePolicyCount_TextField.stringValue = "\(self.remotePolicyCount)"
+                            }
+//                            self.POSTsuccessCount += 1
+                        } else {
+                            print("[RemoveEndpoints] **** Failed to remove: \(policyName)\n")
+
+//                            if self.debug { self.writeToLog(stringOfText: "\n\n") }
+//                            if self.debug { self.writeToLog(stringOfText: "[RemoveEndpoints] ---------- endpoint info ----------\n") }
+//                            if self.debug { self.writeToLog(stringOfText: "[RemoveEndpoints] Type: \(endpointType)\t Name: \(policyName)\t ID: \(policyId)\n") }
+//                            if self.debug { self.writeToLog(stringOfText: "[RemoveEndpoints] ---------- status code ----------\n") }
+//                            if self.debug { self.writeToLog(stringOfText: "[RemoveEndpoints] \(httpResponse.statusCode)\n") }
+//                            if self.debug { self.writeToLog(stringOfText: "[RemoveEndpoints] ---------- response ----------\n") }
+//                            if self.debug { self.writeToLog(stringOfText: "[RemoveEndpoints] \(httpResponse)\n") }
+//                            if self.debug { self.writeToLog(stringOfText: "[RemoveEndpoints] ---------- response ----------\n\n") }
+                        }   // if httpResponse.statusCode - end
+                    }   // if let httpResponse - end
+
+                        
+                    if currentPolicy == policyCount {
+                        self.spinner.stopAnimation(self)
+                        // check for file that allows deleting data from destination server, delete if found - start
+//                            self.rmDELETE()
+                        // check for file that allows deleting data from destination server, delete if found - end
+                        //self.go_button.isEnabled = true
+                    }
+                    semaphore.signal()
+                })  // let task = session.dataTask - end
+                task.resume()
+                semaphore.wait()
+            }   // theOpQ.addOperation - end
+        }
+        if currentPolicy == policyCount {
+//            nodesMigrated+=1    // ;print("added node: \(localEndPointType) - removeEndpoints")
+            //            print("remove nodes complete: \(nodesMigrated)")
+        }
+    }   // func removeEndpoints - end
+    
+    func toEpochTime(dateString: String) -> Double {
+//        var myDate = ""
+        var epochTime:Double = 0.0
+        
+        if dateString != "" {
+            var policyDate = DateComponents()
+            var indexStartOfText = dateString.index(dateString.startIndex, offsetBy: 0)
+            var indexEndOfText = dateString.index(dateString.startIndex, offsetBy: 4)
+            if case policyDate.year = Int(dateString[indexStartOfText..<indexEndOfText]) {
+                //                print("invalid year")
+                return(-10.0)
+            } else {
+                policyDate.year = Int(dateString[indexStartOfText..<indexEndOfText])
+            }
+//            print("year: \(String(describing: policyDate.year!))")
+            
+            indexStartOfText = dateString.index(dateString.startIndex, offsetBy: 5)
+            indexEndOfText = dateString.index(dateString.startIndex, offsetBy: 7)
+            if case policyDate.month = Int(dateString[indexStartOfText..<indexEndOfText]) {
+                //                print("invalid month")
+                return(-10.0)
+            } else {
+                policyDate.month = Int(dateString[indexStartOfText..<indexEndOfText])
+            }
+//            print("month: \(String(describing: policyDate.month!))")
+            
+            indexStartOfText = dateString.index(dateString.startIndex, offsetBy: 8)
+            indexEndOfText = dateString.index(dateString.startIndex, offsetBy: 10)
+            if case policyDate.day = Int(dateString[indexStartOfText..<indexEndOfText]) {
+//                print("invalid day")
+                return(-10.0)
+            } else {
+                policyDate.day = Int(dateString[indexStartOfText..<indexEndOfText])
+                
+            }
+//            print("day: \(String(describing: policyDate.day!))")
+            
+            
+            let dD = Calendar.current.date(from: policyDate)!
+            epochTime = dD.timeIntervalSince1970
+//            print("epochTime: \(epochTime)")
+            
+            // find the oldest policy date
+            if epochTime < oldestRemotePolicyEpoch {
+                oldestRemotePolicyEpoch = epochTime
+                if policyDate.month! < 10 {
+                    monthString = "0\(policyDate.month!)"
+                } else {
+                    monthString = "\(policyDate.month!)"
+                }
+                if policyDate.day! < 10 {
+                    dayString = "0\(policyDate.day!)"
+                } else {
+                    dayString = "\(policyDate.day!)"
+                }
+                oldestRemotePolicyString = "\(String(describing: policyDate.year!))-\(monthString)-\(dayString)"
+            }
+            
+        }
+        return(epochTime)
+    }
+    
+    func alert_dialog(header: String, message: String) {
+        let dialog: NSAlert = NSAlert()
+        dialog.messageText = header
+        dialog.informativeText = message
+        dialog.alertStyle = NSAlert.Style.warning
+        dialog.addButton(withTitle: "OK")
+        dialog.runModal()
+    }   // func alert_dialog - end
+    
+    func urlSession(_ session: URLSession, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping(  URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
+        completionHandler(.useCredential, URLCredential(trust: challenge.protectionSpace.serverTrust!))
+    }
 }
 
